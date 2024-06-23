0b108213f7cb7a434176afdb771dc72aef69c189
ISMAIL <alamismail1@gmail.com>
2024-06-23 10:33:26 -0700

    Initial commit with a basic calculation .py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 1
+number2 = 2
+sum = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+print("Addition of ", number1, "and ", number2, "is ", sum)
+print("Subtraction of ", number1, "and ", number2, "is ", subtraction)
+print("Multiplication of ", number1, "and ", number2, "is ", multiplication)
+
