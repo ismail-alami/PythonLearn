7555fea9ffcecf716e4917af2228b3e2e8227ac4
ISMAIL <alamismail1@gmail.com>
2024-06-30 15:55:00 -0700

    add different scripts

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+result = None
+error_message = None
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        if num2 == 0:
+            error_message = "Error: Division by zero is not allowed."
+        else:
+            result = num1 / num2
+    case _:
+        error_message = "Invalid operation. Please select +, -, * or /."
+if error_message:
+    print(error_message)
+else:
+    print(f"The result is {result}")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+size = int(input("Enter the size of the pattern: "))
+while size <= 0:
+        print("Please enter a positive integer.")
+        size = int(input("Enter the size of the pattern: "))
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
