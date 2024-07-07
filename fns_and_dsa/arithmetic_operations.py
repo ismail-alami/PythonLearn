fb3aff63a62260f6bf9f4a2facfcf0dede44adea
ISMAIL <alamismail1@gmail.com>
2024-07-07 14:01:00 -0700

    add operations function import

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print("Division by 0 is not possible.")
+        case _:
+            print("Please choose 1 of the 4 operations.")
+
