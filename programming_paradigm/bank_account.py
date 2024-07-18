2037c20cc149380ac98e89d86e1e52be2e541d1d
ISMAIL <alamismail1@gmail.com>
2024-07-18 14:47:51 -0700

    add oop scripts 1 fix2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance, amount)
+    def __init__(self, account_balance, amount):
         self.amount = amount
         self.account_balance = account_balance
 
@@ -8,8 +8,8 @@ class BankAccount:
         return self.account_balance
 
     def withdraw(self):
-        if (self.account_balance - amount) > 0:
-            self.account_balance -= amount
+        if (self.account_balance - self.amount) > 0:
+            self.account_balance -= self.amount
             return True
         else:
             return False
