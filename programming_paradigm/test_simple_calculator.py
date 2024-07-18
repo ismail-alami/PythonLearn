76d59d6381ac1e2d525eb06938c001a84f68cb62
ISMAIL <alamismail1@gmail.com>
2024-07-18 16:13:23 -0700

    add third script oop 2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,15 +7,15 @@ class testCalc(unittest.TestCase):
         """initialize the class"""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(4, 1), 5)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(60,-50), 110)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(6,0), 0)
 
-    def test_subtract(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(6,6), 1)
         self.assertEqual(self.calc.divide(6,0),None)
