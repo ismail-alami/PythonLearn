918508dffa6ffa52ffa664c8a4c1d1b8a7ce85b8
ISMAIL <alamismail1@gmail.com>
2024-07-18 14:45:16 -0700

    add oop scripts 1 fix

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,5 +15,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Your balance is {self.account_balance}. Thank you!")
+        print(f"Current Balance: {self.account_balance}. Thank you!")
 
